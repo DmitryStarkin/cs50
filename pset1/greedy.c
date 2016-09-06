c90198be4338ccb61cc46c4ff5813f464b42ebeb
Dmitriy <380506803080@yandex.ua>
2016-09-06 10:15:23 -0400

    mod greedy

--- pset1/greedy.c
+++ pset1/greedy.c
@@ -1,37 +1,31 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <math.h>
+
 int main(void)
 { 
-    const int NOMINAL1=25;
-    const int NOMINAL2=10;
-    const int NOMINAL3=5;
-    const int NOMINAL4=1;
-    int issue=0;
+    const int VALUESofCOINS[]={25,10,5,1}; 
+    int curentValue=0;
+    int coins=0;
     float summa=0;
+    
     while(summa<1) { 
         printf("hai! How much change is owed?\n");
         summa = GetFloat();
         summa = summa*100;
     }
-    int cent = (int) round(summa);
-    while(cent>=NOMINAL1)
-    {
-        cent= cent-NOMINAL1;
-        issue++; 
+    
+    int amountInCents = (int) round(summa);
+    
+    while(amountInCents){
+    
+        while(amountInCents>=VALUESofCOINS[curentValue])
+            {
+                amountInCents -= VALUESofCOINS[curentValue];
+                coins++; 
+            }
+        curentValue++;
     }
-    while(cent>=NOMINAL2)
-    {
-        cent= cent-NOMINAL2;
-        issue++; }
-    while(cent>=NOMINAL3)
-    {
-        cent= cent-NOMINAL3;
-        issue++;}
-    while(cent>=NOMINAL4)
-    {
-        cent= cent-NOMINAL4;
-        issue++; 
-    }
-    printf("%d\n", issue); 
+   
+    printf("%d\n", coins); 
 }

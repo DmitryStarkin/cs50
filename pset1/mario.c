27ed6298e53ad6e87e7d954e6c95d37875182082
Dmitriy <380506803080@yandex.ua>
2016-09-06 06:11:14 -0400

    initial project version

---
+++ pset1/mario.c
@@ -0,0 +1,21 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height=-1;
+    while(height<0 || height>23) {
+        printf("hello, write integer up to 23\n");
+        height = GetInt();
+    }
+    for(int line=0; line<height; line++) {
+        for(int space=1; space<height-line; space++){
+            printf(" ");
+        }
+        for(int sharp=0; sharp<(height-(height-line)+1); sharp++){
+            printf("#");
+        }
+        printf("#\n");
+    }
+    return 0;
+}
---
+++ pset1/water.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+  const int BottlesPerMin = 12;
+  int minutes=0;
+   
+  printf("minutes:");
+  minutes = GetInt();
+  printf("bottles: %d\n", BottlesPerMin*minutes);
+  return 0;
+}

c9904388e04cc5932a030b86c1daeb5fdafad689
Dmitriy <380506803080@yandex.ua>
2016-09-09 03:46:38 -0400

    add pset2 yet without folders hacker

---
+++ pset2/caesar.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<cs50.h>
+#include<stdlib.h>
+
+    int main(int argc, string argv[]) {
+    if (argc!=2) {
+        printf("Error\n");
+        return 1;
+    }
+    int key= atoi(argv[1]);
+    string text = GetString();
+    for (int i=0, len=strlen(text); i<len; i++) {
+        if (isalpha(text[i])){
+            if (isupper(text[i])) printf("%c", (((text[i]-64)+key)%26)+64);
+            else printf("%c", (((text[i]-96)+key)%26)+96);
+        }else printf("%c", text[i]);
+    }
+    printf("\n");
+return 0;
+}
---
+++ pset2/vigenere.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<cs50.h>
+#include<stdlib.h>
+
+int main(int argc, string argv[]) {
+    if (argc!=2) {
+        printf("Error\n");
+        return 1;
+    }
+    
+    string keyword = argv[1];
+    
+    for (int i=0, len=strlen(keyword); i<len; i++){
+        if (!isalpha(keyword[i])) {
+            printf("Error\n");
+            return 1;
+        }
+    }
+ 
+    string text = GetString();
+    
+    for (int i=0, counter=0, key, len=strlen(text); i<len; i++) {
+        if (isalpha(text[i])){
+            if (counter==strlen(keyword)) counter=0;
+            if (isupper(keyword[counter])) key=(int) keyword[counter]-65;
+            else key=(int) keyword[counter]-97;
+            if (isupper(text[i]))printf("%c", (((text[i]-64)+key)%26)+64);
+            else printf("%c", (((text[i]-96)+key)%26)+96);
+            counter++;
+        }else printf("%c", text[i]);
+    }
+printf("\n");
+return 0;
+}
